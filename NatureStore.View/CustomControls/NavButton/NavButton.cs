af235f4eb511f96f7989bc212d0197a458b739cb
asafnr123 <56031984+asafnr123@users.noreply.github.com>
2022-11-29 16:52:38 +0200

    added homePage and a nav button cusom control

---
+++ NatureStore.View/CustomControls/NavButton/NavButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NatureStore.View.CustomControls.NavButton
+{
+
+    public class NavButton : ListBoxItem
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+
+        public Uri? Link { get; set; }
+        public Geometry? Icon { get; set; }
+
+    }
+}
