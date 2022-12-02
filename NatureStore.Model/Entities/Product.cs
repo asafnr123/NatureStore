50407fcfbc83f31cd27ca422d913c818ca748890
Asaf <asafnr567@gmail.com>
2022-12-03 01:55:27 +0200

    added migration for product brand and added some pics for protein

--- NatureStore.Model/Entities/Product.cs
+++ NatureStore.Model/Entities/Product.cs
@@ -13,5 +13,6 @@ namespace NatureStore.Model.Entitys
         public Category Category { get; set; }
         public float Price { get; set; }
         public string Description { get; set; }
+        public string Brand { get; set; }
     }
 }
