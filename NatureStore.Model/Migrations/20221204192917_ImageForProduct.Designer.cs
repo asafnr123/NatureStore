45cd71aa88e25611b3074ed88b2cf3c72d85760c
Asaf <asafnr567@gmail.com>
2022-12-04 21:31:36 +0200

    added image column for product

--- NatureStore.Model/Migrations/20221204192917_ImageForProduct.Designer.cs
+++ NatureStore.Model/Migrations/20221204192917_ImageForProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NatureStore.Model.Context;
 
@@ -11,9 +12,11 @@ using NatureStore.Model.Context;
 namespace NatureStore.Model.Migrations
 {
     [DbContext(typeof(NatureStoreDbContext))]
-    partial class NatureStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221204192917_ImageForProduct")]
+    partial class ImageForProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
