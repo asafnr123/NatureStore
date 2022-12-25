73375f7d13b6249a9f3f4f135511bde341964ce8
asafnr123 <56031984+asafnr123@users.noreply.github.com>
2022-12-25 21:47:05 +0200

    zsc

--- NatureStore.Model/Context/NatureStoreDbContext.cs
+++ NatureStore.Model/Context/NatureStoreDbContext.cs
@@ -21,7 +21,7 @@ namespace NatureStore.Model.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Data Source=2a06:c701:4346:6b00:716d:a7ab:e9de:82ea;Network Library=DBMSSOCN;Initial Catalog=NatureStoreDb;User Id=anyuser;Password=Ns1213;;TrustServerCertificate=True;");
+            builder.UseSqlServer("Data Source=2a06:c701:4346:6b00:716d:a7ab:e9de:82ea;Initial Catalog=NatureStoreDb;User Id=anyuser;Password=Ns1213;;TrustServerCertificate=True;");
         }
         
     }
