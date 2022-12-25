7448e2c5ed51e06c5f4a6273e7a6e9ec13b3a55f
Asaf <asafnr567@gmail.com>
2022-12-25 15:58:20 +0200

    fixed connection string with ipv6

--- NatureStore.Model/Context/NatureStoreDbContext.cs
+++ NatureStore.Model/Context/NatureStoreDbContext.cs
@@ -21,7 +21,7 @@ namespace NatureStore.Model.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Data Source=10.0.0.35,1433;Initial Catalog=NatureStoreDb;User ID=anyuser;Password=Ns1213;TrustServerCertificate=True");
+            builder.UseSqlServer("Data Source=2a06:c701:4346:6b00:f924:8c06:294:2872,1433;Initial Catalog=NatureStoreDb;User ID=anyuser;Password=Ns1213;TrustServerCertificate=True;");
         }
         
     }
