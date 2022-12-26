f6b0af46a6b32b4186ce1681c23e61a290c8a007
Asaf <asafnr567@gmail.com>
2022-12-26 22:47:11 +0200

    awd

--- NatureStore.Model/Context/NatureStoreDbContext.cs
+++ NatureStore.Model/Context/NatureStoreDbContext.cs
@@ -3,6 +3,7 @@ using NatureStore.Model.Entities;
 using NatureStore.Model.Entitys;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace NatureStore.Model.Context
 {
     public class NatureStoreDbContext : DbContext
     {
+        private static int counter = 0;
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Stock> Stocks { get; set; }    
@@ -21,9 +23,24 @@ namespace NatureStore.Model.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Data Source=tcp:147.235.207.55,1433;Initial Catalog=NatureStoreDb;User Id=anyuser;Password=Ns1213;;TrustServerCertificate=True;");
+            builder.UseSqlServer("Data Source=(local);Initial Catalog=NatureStoreDb;User Id=anyuser;Password=Ns1213;;TrustServerCertificate=True;");
+            if(counter == 0)
+            {
+                counter++;
+                CreateDbLocally();
+            }
+
         }
-        
+
+        public void CreateDbLocally()
+        {
+            using (var client = new NatureStoreDbContext())
+            {
+                client.Database.EnsureCreated();
+            }
+        }
+
+
     }
 
     
