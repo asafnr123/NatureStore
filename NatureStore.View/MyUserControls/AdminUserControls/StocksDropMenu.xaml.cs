fe9d29ba0796cb497900e3f186a37ac88bed97d7
Asaf <asafnr567@gmail.com>
2022-12-31 20:18:52 +0200

    created stop drop menu

---
+++ NatureStore.View/MyUserControls/AdminUserControls/StocksDropMenu.xaml.cs
@@ -0,0 +1,50 @@
+﻿using NatureStore.Controller;
+using NatureStore.View.Pages.AdminHomePage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NatureStore.View.MyUserControls.AdminUserControls
+{
+    /// <summary>
+    /// Interaction logic for StocksDropMenu.xaml
+    /// </summary>
+    public partial class StocksDropMenu : UserControl
+    {
+        public StocksDropMenu()
+        {
+            InitializeComponent();
+        }
+
+        public AdminPage adminPage { private get; set; }
+        private readonly DbReader reader = new();
+
+        private void allCateLbl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            adminPage.myDataGrid.ItemsSource = null;
+            adminPage.myDataGrid.ItemsSource ??= reader.GetAllStocks();
+            adminPage.MainAdminFrame.Visibility = Visibility.Hidden;
+            adminPage.stocks.Visibility = Visibility.Hidden;
+            adminPage.myDataGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Add_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            adminPage.myDataGrid.Visibility = Visibility.Collapsed;
+            adminPage.MainAdminFrame.Navigate(null);
+            adminPage.stocksWP.Visibility = Visibility.Hidden;
+            adminPage.MainAdminFrame.Visibility = Visibility.Visible;
+        }
+    }
+}
