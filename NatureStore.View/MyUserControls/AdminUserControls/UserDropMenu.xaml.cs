79cba995c3f6526c0c8bd24fa925b9d079d9c68d
Asaf <asafnr567@gmail.com>
2023-01-01 20:34:06 +0200

    changed all admin drop menus

--- NatureStore.View/MyUserControls/AdminUserControls/UserDropMenu.xaml.cs
+++ NatureStore.View/MyUserControls/AdminUserControls/UserDropMenu.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using NatureStore.Controller;
 using NatureStore.View.Pages.AdminHomePage;
-using NatureStore.View.Pages.UserHomePage;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace NatureStore.View.MyUserControls.AdminUserControls
 {
